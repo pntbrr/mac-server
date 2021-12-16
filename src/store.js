7b73721a4039d251811058186438fdc5aaf2019f
leoboyerbx <contact@leoboyer.fr>
2021-12-16 14:04:51 +0100

    wip

--- src/store.js
+++ src/store.js
@@ -3,6 +3,7 @@ import { createPinia, defineStore } from 'pinia'
 export const pinia = createPinia()
 export const useStore = defineStore('main', {
     state: () => ({
+        currentStep: 'idle',
         feetAnimSpeed: 0,
         sundialAnim: {
             duration: 0,
